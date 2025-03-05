b40667194d72bc02108881b6b6a9d15833af46d6
Aman Singh <77951577+Amans66@users.noreply.github.com>
2025-03-05 15:17:48 +0530

    ShieldCrypt

--- ShieldCrypt/key_decryption.py
+++
@@ -1,9 +0,0 @@
-def custom_decrypt(data):
-    """Reverses ROT13 for letters and restores numbers/symbols."""
-    reverse_mapping = {
-        **{chr(((i - 65 + 13) % 26) + 65): chr(i) for i in range(65, 91)},  # A-Z ROT13 reverse
-        **{chr(((i - 97 + 13) % 26) + 97): chr(i) for i in range(97, 123)},  # a-z ROT13 reverse
-        '!': '1', '@': '2', '#': '3', '$': '4', '%': '5', '^': '6', '&': '7', '*': '8', '(': '9', ')': '0',
-        '0': '!', '9': '@', '8': '#', '7': '$', '6': '%', '5': '^', '4': '&', '3': '*', '2': '(', '1': ')'
-    }
-    return ''.join(reverse_mapping.get(c, c) for c in data)
--- ShieldCrypt/key_encryption.py
+++
@@ -1,9 +0,0 @@
-def custom_encrypt(data):
-    """Applies ROT13 to letters and custom substitution for numbers/symbols."""
-    mapping = {
-        **{chr(i): chr(((i - 65 + 13) % 26) + 65) for i in range(65, 91)},  # A-Z ROT13 (Proper Wrapping)
-        **{chr(i): chr(((i - 97 + 13) % 26) + 97) for i in range(97, 123)},  # a-z ROT13 (Proper Wrapping)
-        '1': '!', '2': '@', '3': '#', '4': '$', '5': '%', '6': '^', '7': '&', '8': '*', '9': '(', '0': ')',
-        '!': '1', '@': '2', '#': '3', '$': '4', '%': '5', '^': '6', '&': '7', '*': '8', '(': '9', ')': '0'
-    }
-    return ''.join(mapping.get(c, c) for c in data)
